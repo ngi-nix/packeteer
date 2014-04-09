ebcfdafd50182ca34a3fe4696860444caf82a81b
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-04-09 13:45:31 +0200

    Simplify and/or bring up-to-date the build script

--- common.mk
+++ common.mk
@@ -2,15 +2,12 @@
 AM_CXXFLAGS = -I$(top_srcdir) @AM_CXXFLAGS@
 
 # Set default compiler flags
-AM_CXXFLAGS += -g --std=c++0x
-
-# Set debugging-related options.
-if DEBUG
-AM_CXXFLAGS += -DDEBUG -O0
-else
-AM_CXXFLAGS += -DNDEBUG -O3
-endif
+AM_CXXFLAGS += -g -std=c++0x -Wc++0x-compat
 
 # Dependencies XXX keep these in sync with packetflinger.pc.in
-AM_CXXFLAGS += $(BOOST_CPPFLAGS)
-# AM_LDFLAGS += $(BOOST_SIGNALS_LIB)
+AM_CXXFLAGS += $(META_CFLAGS) $(TWINE_CFLAGS)
+# AM_LDFLAGS += 
+
+# Build object files in subdirectories so we can support platform-dependent
+# builds
+AUTOMAKE_OPTIONS = subdir-objects
