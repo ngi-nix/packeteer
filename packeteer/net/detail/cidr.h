f9d40dfa6ffce6b653fce323956b7e79782f85d5
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-06-17 14:14:34 +0200

    Updated copyright notice. Note that Tom Thomas of CrowdRoaming granted permission to use this code that (in a slightly different form) I contributed to their project. Check out CrowdRoaming, their social bandwidth concept is awesome! (http://www.crowdroaming.com/)

--- packeteer/net/detail/cidr.h
+++ packeteer/net/detail/cidr.h
@@ -1,12 +1,22 @@
-//
-//              Project Name: CrowdRoaming
-//              file: cidr.hpp
-//              copyright: © 2013 DASHT BV, unpublished work.
-//                             This computer program includes Confidential, Proprietary Information and is a Trade Secret
-//                             of DASHT BV. All use, disclosure, and/or
-//                             reproduction is prohibited unless authorized in writing. All Rights Reserved.
-//
-//
+/**
+ * This file is part of packeteer.
+ *
+ * Author(s): Jens Finkhaeuser <jens@unwesen.co.uk>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
 
 #ifndef PACKETEER_NET_DETAIL_CIDR_H
 #define PACKETEER_NET_DETAIL_CIDR_H
