9e885c3eb129fb0fe3269f76a1280fdb7c3c911d
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-12-18 09:25:14 +0100

    WIP; fix connector tests

--- test/test_connector.cpp
+++ test/test_connector.cpp
@@ -31,6 +31,8 @@
 
 #include <twine/chrono.h>
 
+#include "value_tests.h"
+
 
 using namespace packeteer;
 
@@ -38,106 +40,106 @@ namespace {
 
 struct test_data
 {
-  char const *              address;
-  bool                      valid;
-  connector::connector_type type;
+  char const *    address;
+  bool            valid;
+  connector_type  type;
 } tests[] = {
   // Garbage
-  { "foo", false, connector::CT_UNSPEC },
-  { "foo:", false, connector::CT_UNSPEC },
-  { "foo://", false, connector::CT_UNSPEC },
-  { "foo:///some/path", false, connector::CT_UNSPEC },
-  { "foo://123.123.133.123:12", false, connector::CT_UNSPEC },
-  { "tcp://foo", false, connector::CT_UNSPEC },
-  { "tcp4://foo", false, connector::CT_UNSPEC },
-  { "tcp6://foo", false, connector::CT_UNSPEC },
-  { "udp://foo", false, connector::CT_UNSPEC },
-  { "udp4://foo", false, connector::CT_UNSPEC },
-  { "udp6://foo", false, connector::CT_UNSPEC },
-  { "file://", false, connector::CT_UNSPEC },
-  { "ipc://", false, connector::CT_UNSPEC },
-  { "pipe://", false, connector::CT_UNSPEC },
-  { "anon://anything/here", false, connector::CT_UNSPEC },
+  { "foo", false, CT_UNSPEC },
+  { "foo:", false, CT_UNSPEC },
+  { "foo://", false, CT_UNSPEC },
+  { "foo:///some/path", false, CT_UNSPEC },
+  { "foo://123.123.133.123:12", false, CT_UNSPEC },
+  { "tcp://foo", false, CT_UNSPEC },
+  { "tcp4://foo", false, CT_UNSPEC },
+  { "tcp6://foo", false, CT_UNSPEC },
+  { "udp://foo", false, CT_UNSPEC },
+  { "udp4://foo", false, CT_UNSPEC },
+  { "udp6://foo", false, CT_UNSPEC },
+  { "file://", false, CT_UNSPEC },
+  { "ipc://", false, CT_UNSPEC },
+  { "pipe://", false, CT_UNSPEC },
+  { "anon://anything/here", false, CT_UNSPEC },
 
   // IPv4 hosts
-  { "tcp://192.168.0.1",      true, connector::CT_TCP },
-  { "tcp://192.168.0.1:8080", true, connector::CT_TCP },
-  { "tCp://192.168.0.1",      true, connector::CT_TCP },
-  { "tcP://192.168.0.1:8080", true, connector::CT_TCP },
+  { "tcp://192.168.0.1",      true, CT_TCP },
+  { "tcp://192.168.0.1:8080", true, CT_TCP },
+  { "tCp://192.168.0.1",      true, CT_TCP },
+  { "tcP://192.168.0.1:8080", true, CT_TCP },
 
-  { "tcp4://192.168.0.1",      true, connector::CT_TCP4 },
-  { "tcp4://192.168.0.1:8080", true, connector::CT_TCP4 },
-  { "tCp4://192.168.0.1",      true, connector::CT_TCP4 },
-  { "tcP4://192.168.0.1:8080", true, connector::CT_TCP4 },
+  { "tcp4://192.168.0.1",      true, CT_TCP4 },
+  { "tcp4://192.168.0.1:8080", true, CT_TCP4 },
+  { "tCp4://192.168.0.1",      true, CT_TCP4 },
+  { "tcP4://192.168.0.1:8080", true, CT_TCP4 },
 
-  { "tcp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, connector::CT_UNSPEC, },
-  { "tcp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, connector::CT_UNSPEC, },
-  { "tcp4://2001:0db8:85a3::8a2e:0370:7334",             false, connector::CT_UNSPEC, },
-  { "Tcp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, connector::CT_UNSPEC, },
-  { "tCp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, connector::CT_UNSPEC, },
-  { "tcP4://2001:0db8:85a3::8a2e:0370:7334",             false, connector::CT_UNSPEC, },
+  { "tcp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, CT_UNSPEC, },
+  { "tcp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, CT_UNSPEC, },
+  { "tcp4://2001:0db8:85a3::8a2e:0370:7334",             false, CT_UNSPEC, },
+  { "Tcp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, CT_UNSPEC, },
+  { "tCp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, CT_UNSPEC, },
+  { "tcP4://2001:0db8:85a3::8a2e:0370:7334",             false, CT_UNSPEC, },
 
-  { "udp://192.168.0.1",      true, connector::CT_UDP },
-  { "udp://192.168.0.1:8080", true, connector::CT_UDP },
-  { "uDp://192.168.0.1",      true, connector::CT_UDP },
-  { "udP://192.168.0.1:8080", true, connector::CT_UDP },
+  { "udp://192.168.0.1",      true, CT_UDP },
+  { "udp://192.168.0.1:8080", true, CT_UDP },
+  { "uDp://192.168.0.1",      true, CT_UDP },
+  { "udP://192.168.0.1:8080", true, CT_UDP },
 
-  { "udp4://192.168.0.1",      true, connector::CT_UDP4 },
-  { "udp4://192.168.0.1:8080", true, connector::CT_UDP4 },
-  { "uDp4://192.168.0.1",      true, connector::CT_UDP4 },
-  { "udP4://192.168.0.1:8080", true, connector::CT_UDP4 },
+  { "udp4://192.168.0.1",      true, CT_UDP4 },
+  { "udp4://192.168.0.1:8080", true, CT_UDP4 },
+  { "uDp4://192.168.0.1",      true, CT_UDP4 },
+  { "udP4://192.168.0.1:8080", true, CT_UDP4 },
 
-  { "udp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, connector::CT_UNSPEC, },
-  { "udp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, connector::CT_UNSPEC, },
-  { "udp4://2001:0db8:85a3::8a2e:0370:7334",             false, connector::CT_UNSPEC, },
-  { "Udp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, connector::CT_UNSPEC, },
-  { "uDp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, connector::CT_UNSPEC, },
-  { "udP4://2001:0db8:85a3::8a2e:0370:7334",             false, connector::CT_UNSPEC, },
+  { "udp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, CT_UNSPEC, },
+  { "udp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, CT_UNSPEC, },
+  { "udp4://2001:0db8:85a3::8a2e:0370:7334",             false, CT_UNSPEC, },
+  { "Udp4://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    false, CT_UNSPEC, },
+  { "uDp4://2001:0db8:85a3:0:0:8a2e:0370:7334",          false, CT_UNSPEC, },
+  { "udP4://2001:0db8:85a3::8a2e:0370:7334",             false, CT_UNSPEC, },
 
   // IPv6 hosts
-  { "tcp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_TCP, },
-  { "tcp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_TCP, },
-  { "tcp://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_TCP, },
-  { "Tcp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_TCP, },
-  { "tCp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_TCP, },
-  { "tcP://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_TCP, },
+  { "tcp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_TCP, },
+  { "tcp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_TCP, },
+  { "tcp://2001:0db8:85a3::8a2e:0370:7334",             true, CT_TCP, },
+  { "Tcp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_TCP, },
+  { "tCp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_TCP, },
+  { "tcP://2001:0db8:85a3::8a2e:0370:7334",             true, CT_TCP, },
 
-  { "tcp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_TCP6, },
-  { "tcp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_TCP6, },
-  { "tcp6://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_TCP6, },
-  { "Tcp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_TCP6, },
-  { "tCp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_TCP6, },
-  { "tcP6://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_TCP6, },
+  { "tcp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_TCP6, },
+  { "tcp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_TCP6, },
+  { "tcp6://2001:0db8:85a3::8a2e:0370:7334",             true, CT_TCP6, },
+  { "Tcp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_TCP6, },
+  { "tCp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_TCP6, },
+  { "tcP6://2001:0db8:85a3::8a2e:0370:7334",             true, CT_TCP6, },
 
-  { "tcp6://192.168.0.1",      false, connector::CT_UNSPEC },
-  { "tcp6://192.168.0.1:8080", false, connector::CT_UNSPEC },
-  { "tCp6://192.168.0.1",      false, connector::CT_UNSPEC },
-  { "tcP6://192.168.0.1:8080", false, connector::CT_UNSPEC },
+  { "tcp6://192.168.0.1",      false, CT_UNSPEC },
+  { "tcp6://192.168.0.1:8080", false, CT_UNSPEC },
+  { "tCp6://192.168.0.1",      false, CT_UNSPEC },
+  { "tcP6://192.168.0.1:8080", false, CT_UNSPEC },
 
-  { "udp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_UDP, },
-  { "udp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_UDP, },
-  { "udp://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_UDP, },
-  { "Udp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_UDP, },
-  { "uDp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_UDP, },
-  { "udP://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_UDP, },
+  { "udp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_UDP, },
+  { "udp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_UDP, },
+  { "udp://2001:0db8:85a3::8a2e:0370:7334",             true, CT_UDP, },
+  { "Udp://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_UDP, },
+  { "uDp://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_UDP, },
+  { "udP://2001:0db8:85a3::8a2e:0370:7334",             true, CT_UDP, },
 
-  { "udp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_UDP6, },
-  { "udp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_UDP6, },
-  { "udp6://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_UDP6, },
-  { "Udp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, connector::CT_UDP6, },
-  { "uDp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, connector::CT_UDP6, },
-  { "udP6://2001:0db8:85a3::8a2e:0370:7334",             true, connector::CT_UDP6, },
+  { "udp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_UDP6, },
+  { "udp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_UDP6, },
+  { "udp6://2001:0db8:85a3::8a2e:0370:7334",             true, CT_UDP6, },
+  { "Udp6://2001:0db8:85a3:0000:0000:8a2e:0370:7334",    true, CT_UDP6, },
+  { "uDp6://2001:0db8:85a3:0:0:8a2e:0370:7334",          true, CT_UDP6, },
+  { "udP6://2001:0db8:85a3::8a2e:0370:7334",             true, CT_UDP6, },
 
-  { "udp6://192.168.0.1",      false, connector::CT_UNSPEC },
-  { "udp6://192.168.0.1:8080", false, connector::CT_UNSPEC },
-  { "udP6://192.168.0.1",      false, connector::CT_UNSPEC },
-  { "uDp6://192.168.0.1:8080", false, connector::CT_UNSPEC },
+  { "udp6://192.168.0.1",      false, CT_UNSPEC },
+  { "udp6://192.168.0.1:8080", false, CT_UNSPEC },
+  { "udP6://192.168.0.1",      false, CT_UNSPEC },
+  { "uDp6://192.168.0.1:8080", false, CT_UNSPEC },
 
   // All other types require path names. There's not much common
   // about path names, so our only requirement is that it exists.
-  { "local://foo", true, connector::CT_LOCAL },
-  { "pipe://foo", true, connector::CT_PIPE },
-  { "anon://", true, connector::CT_ANON },
+  { "local://foo", true, CT_LOCAL },
+  { "pipe://foo", true, CT_PIPE },
+  { "anon://", true, CT_ANON },
 
 };
 
@@ -150,7 +152,7 @@ public:
   CPPUNIT_TEST_SUITE(ConnectorTest);
 
     CPPUNIT_TEST(testAddressParsing);
-    CPPUNIT_TEST(testCopySemantics);
+    CPPUNIT_TEST(testValueSemantics);
     CPPUNIT_TEST(testAnonConnector);
     CPPUNIT_TEST(testLocalConnector);
     CPPUNIT_TEST(testPipeConnector);
@@ -181,11 +183,11 @@ private:
 
 
 
-  void testCopySemantics()
+  void testValueSemantics()
   {
     // We'll use an anon connector, because they're simplest.
     connector original("anon://");
-    CPPUNIT_ASSERT_EQUAL(connector::CT_ANON, original.type());
+    CPPUNIT_ASSERT_EQUAL(CT_ANON, original.type());
 
     connector copy = original;
     CPPUNIT_ASSERT_EQUAL(original.type(), copy.type());
@@ -195,15 +197,7 @@ private:
     CPPUNIT_ASSERT(original == copy);
     CPPUNIT_ASSERT(!(original < copy));
 
-    original.swap(copy);
-    CPPUNIT_ASSERT(original == copy);
-
-    std::swap(original, copy);
-    CPPUNIT_ASSERT(original == copy);
-
-    CPPUNIT_ASSERT(std::equal_to<connector>()(original, copy));
-    CPPUNIT_ASSERT(!std::less<connector>()(original, copy));
-    CPPUNIT_ASSERT_EQUAL(std::hash<connector>()(original), copy.hash());
+    PACKETEER_VALUES_TEST(copy, original, true);
   }
 
 
@@ -236,16 +230,22 @@ private:
     // for that, address needs to not return a string (grr)
     std::string msg = "hello, world!";
     size_t amount = 0;
-    CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, sender.write(msg.c_str(), msg.size(), amount));
+    std::cout << "from " << sender.address() << " to " << receiver.address() << std::endl;
+    CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, sender.send(msg.c_str(), msg.size(), amount,
+        receiver.address()));
     CPPUNIT_ASSERT_EQUAL(msg.size(), amount);
 
     twine::chrono::sleep(twine::chrono::milliseconds(50));
 
     std::vector<char> result;
     result.reserve(2 * msg.size());
-    CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, receiver.read(&result[0], result.capacity(),
-          amount));
+    packeteer::net::socket_address sendaddr;
+    std::cout << "receiving..." << std::endl;
+    CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, receiver.receive(&result[0], result.capacity(),
+          amount, sendaddr));
     CPPUNIT_ASSERT_EQUAL(msg.size(), amount);
+    std::cout << "sender socket address: " << sender.socket_address().cidr_str() << std::endl;
+    CPPUNIT_ASSERT_EQUAL(sender.socket_address().cidr_str(), sendaddr.cidr_str());
 
     for (size_t i = 0 ; i < msg.size() ; ++i) {
       CPPUNIT_ASSERT_EQUAL(msg[i], result[i]);
@@ -254,8 +254,7 @@ private:
 
 
 
-  void testStreamConnector(connector::connector_type expected_type,
-      std::string const & addr)
+  void testStreamConnector(connector_type expected_type, std::string const & addr)
   {
     // Tests for "stream" connectors, i.e. connectors that allow synchronous,
     // reliable delivery.
@@ -297,8 +296,7 @@ private:
 
 
 
-  void testDGramConnector(connector::connector_type expected_type,
-      std::string const & addr)
+  void testDGramConnector(connector_type expected_type, std::string const & addr)
   {
     // Tests for "datagram" connectors, i.e. connectors that allow synchronous,
     // un-reliable delivery.
@@ -311,6 +309,7 @@ private:
 
     CPPUNIT_ASSERT(!server.listening());
     CPPUNIT_ASSERT(!server.connected());
+
 
     CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, server.listen());
 
@@ -326,18 +325,20 @@ private:
     CPPUNIT_ASSERT(!client.listening());
     CPPUNIT_ASSERT(!client.connected());
 
+//    CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, client.listen());
     CPPUNIT_ASSERT_EQUAL(ERR_SUCCESS, client.connect());
-    connector server_conn = server.accept();
-
-    twine::chrono::sleep(twine::chrono::milliseconds(50));
 
     CPPUNIT_ASSERT(!client.listening());
     CPPUNIT_ASSERT(client.connected());
-    CPPUNIT_ASSERT(server_conn.listening());
+
+    twine::chrono::sleep(twine::chrono::milliseconds(50));
 
     // Communications
-    sendMessageDGram(client, server_conn);
-    sendMessageDGram(server_conn, client);
+    std::cout << "send(client, server)" << std::endl;
+    sendMessageDGram(client, server);
+    std::cout << "send(server, client)" << std::endl;
+    std::cout << "client address " << client.address() << std::endl;
+    sendMessageDGram(server, client);
   }
 
 
@@ -347,7 +348,7 @@ private:
     // Anonymous pipes are special in that they need only one connector for
     // communications.
     connector conn("anon://");
-    CPPUNIT_ASSERT_EQUAL(connector::CT_ANON, conn.type());
+    CPPUNIT_ASSERT_EQUAL(CT_ANON, conn.type());
 
     CPPUNIT_ASSERT(!conn.listening());
     CPPUNIT_ASSERT(!conn.connected());
@@ -378,7 +379,7 @@ private:
   void testLocalConnector()
   {
     // Local sockets are "stream" connectors
-    testStreamConnector(connector::CT_LOCAL, "local:///tmp/test-connector-local");
+    testStreamConnector(CT_LOCAL, "local:///tmp/test-connector-local");
   }
 
 
@@ -386,7 +387,7 @@ private:
   void testPipeConnector()
   {
     // Named pipes are "stream" connectors
-    testStreamConnector(connector::CT_PIPE, "pipe:///tmp/test-connector-pipe");
+    testStreamConnector(CT_PIPE, "pipe:///tmp/test-connector-pipe");
   }
 
 
@@ -394,7 +395,7 @@ private:
   void testTCPv4Connector()
   {
     // TCP over IPv4 to localhost
-    testStreamConnector(connector::CT_TCP4, "tcp4://127.0.0.1:54321");
+    testStreamConnector(CT_TCP4, "tcp4://127.0.0.1:54321");
   }
 
 
@@ -402,7 +403,7 @@ private:
   void testTCPv6Connector()
   {
     // TCP over IPv6 to localhost
-    testStreamConnector(connector::CT_TCP6, "tcp6://[::1]:54321");
+    testStreamConnector(CT_TCP6, "tcp6://[::1]:54321");
   }
 
 
@@ -410,7 +411,7 @@ private:
   void testUDPv4Connector()
   {
     // UDP over IPv4 to localhost
-    testDGramConnector(connector::CT_UDP4, "udp4://127.0.0.1:54321");
+    testDGramConnector(CT_UDP4, "udp4://127.0.0.1:54321");
   }
 
 
@@ -418,7 +419,7 @@ private:
   void testUDPv6Connector()
   {
     // UDP over IPv6 to localhost
-    testDGramConnector(connector::CT_UDP6, "udp6://[::1]:54321");
+    testDGramConnector(CT_UDP6, "udp6://[::1]:54321");
   }
 };
 
