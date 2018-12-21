ab3ea37f33d21fc3743cd8a796a2e4fa127af317
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-12-21 15:46:51 +0100

    Update copyright

--- packeteer/connector_specs.h
+++ packeteer/connector_specs.h
@@ -4,7 +4,7 @@
  * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
  *
  * Copyright (c) 2014 Unwesen Ltd.
- * Copyright (c) 2015-2018 Jens Finkhaeuser.
+ * Copyright (c) 2015-2019 Jens Finkhaeuser.
  *
  * This software is licensed under the terms of the GNU GPLv3 for personal,
  * educational and non-profit use. For all other uses, alternative license
