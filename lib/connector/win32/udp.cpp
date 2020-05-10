0088a90c8b7d9d4c9f7db2aa88d021b098089fff
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-05-10 16:14:15 +0200

    Add (currently not working) UDP implementation

---
+++ lib/connector/win32/udp.cpp
@@ -0,0 +1,111 @@
+/**
+ * This file is part of packeteer.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2020 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#include <build-config.h>
+
+#include "udp.h"
+
+#include <packeteer/net/socket_address.h>
+#include <packeteer/error.h>
+
+#include "../../globals.h"
+
+namespace packeteer::detail {
+
+namespace {
+
+inline int
+select_domain(::packeteer::net::socket_address const & addr)
+{
+  switch (addr.type()) {
+    case ::packeteer::net::AT_INET4:
+      return AF_INET;
+
+    case ::packeteer::net::AT_INET6:
+      return AF_INET6;
+
+    default:
+      throw exception(ERR_INVALID_VALUE, "Expected IPv4 or IPv6 address!");
+  }
+}
+
+} // anonymous namespace
+
+connector_udp::connector_udp(net::socket_address const & addr,
+    connector_options const & options)
+  : connector_socket(addr, options)
+{
+}
+
+
+
+connector_udp::~connector_udp()
+{
+  close();
+}
+
+
+
+error_t
+connector_udp::connect()
+{
+  return connector_socket::socket_connect(select_domain(m_addr), SOCK_DGRAM,
+      IPPROTO_UDP);
+}
+
+
+
+error_t
+connector_udp::listen()
+{
+  // Attempt to bind
+  handle::sys_handle_t handle = handle::INVALID_SYS_HANDLE;
+  error_t err = connector_socket::socket_bind(select_domain(m_addr),
+      SOCK_DGRAM, IPPROTO_UDP, handle);
+  if (ERR_SUCCESS != err) {
+    return err;
+  }
+
+  // Finally, set the handle
+  m_handle = handle;
+  m_server = true;
+
+  return ERR_SUCCESS;
+}
+
+
+
+error_t
+connector_udp::close()
+{
+  return connector_socket::socket_close();
+}
+
+
+connector_interface *
+connector_udp::accept(net::socket_address & /* addr*/)
+{
+  if (!listening()) {
+    return nullptr;
+  }
+  return this;
+}
+
+
+} // namespace packeteer::detail
