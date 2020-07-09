8b88a186d2bbef81bd5fb3713e1582e203423677
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-07-09 10:47:14 +0200

    Add copyright header

--- android.py
+++ android.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# This file is part of packeteer.
+#
+# Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+#
+# Copyright (c) 2020 Jens Finkhaeuser.
+#
+# This software is licensed under the terms of the GNU GPLv3 for personal,
+# educational and non-profit use. For all other uses, alternative license
+# options are available. Please contact the copyright holder for additional
+# information, stating your intended usage.
+#
+# You can find the full text of the GPLv3 in the COPYING file in this code
+# distribution.
+#
+# This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.
 
 def get_host_properties(sdk, abi, platform):
     import os.path
