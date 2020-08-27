3cc5408fcaae86287c2ba88dec6c91c816d6ecc0
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-08-27 13:38:21 +0200

    Test resolver custom schemes

--- test/public/test_resolver.cpp
+++ test/public/test_resolver.cpp
@@ -196,5 +196,85 @@ TEST(Resolver, resolve_udp_with_localhost)
   }
 }
 
-// TODO test cases:
-// - udp_with_localhost -> 1-2, different types
+
+TEST(Resolver, custom_scheme_fails_without_registration)
+{
+  auto api = packeteer::api::create();
+  auto url = l6e::net::url::parse("test-scheme:///foo/bar");
+  std::set<l6e::net::url> results;
+
+  // Without registerting anything, a test url won't be resolved.
+  auto err = api->resolver().resolve(results, url);
+  ASSERT_EQ(p7r::ERR_INVALID_VALUE, err);
+}
+
+
+
+TEST(Resolver, custom_scheme_works_with_registration)
+{
+  auto api = packeteer::api::create();
+  auto url = l6e::net::url::parse("test-scheme:///foo/bar");
+  std::set<l6e::net::url> results;
+
+  // Register a test resolution function, try again.
+  auto err = api->resolver().register_resolution_function("test-scheme",
+      [](std::set<l6e::net::url> & res, l6e::net::url const & query) -> p7r::error_t
+      {
+        auto copy = query;
+        copy.path.replace(1, 3, "quux");
+        res.insert(copy);
+        return p7r::ERR_SUCCESS;
+      }
+  );
+  ASSERT_EQ(p7r::ERR_SUCCESS, err);
+  err = api->resolver().resolve(results, url);
+  ASSERT_EQ(p7r::ERR_SUCCESS, err);
+  ASSERT_EQ(1, results.size());
+  auto res = *results.begin();
+  ASSERT_EQ(res.path, "/quux/bar");
+}
+
+
+TEST(Resolver, custom_scheme_double_registration_fails)
+{
+  auto api = packeteer::api::create();
+
+  auto err = api->resolver().register_resolution_function("test-scheme",
+      [](std::set<l6e::net::url> &, l6e::net::url const &) -> p7r::error_t
+      {
+        return p7r::ERR_SUCCESS;
+      }
+  );
+  ASSERT_EQ(p7r::ERR_SUCCESS, err);
+
+
+  // Registering the same scheme again will fail.
+  err = api->resolver().register_resolution_function("test-scheme",
+      [](std::set<l6e::net::url> &, l6e::net::url const &) -> p7r::error_t
+      {
+        return p7r::ERR_SUCCESS;
+      }
+  );
+  ASSERT_EQ(p7r::ERR_INVALID_VALUE, err);
+}
+
+
+TEST(Resolver, custom_scheme_produces_errors)
+{
+  auto api = packeteer::api::create();
+  auto url = l6e::net::url::parse("error:///foo/bar");
+  std::set<l6e::net::url> results;
+
+  // Registering an erroring scheme, and try to resolve that.
+  auto err = api->resolver().register_resolution_function("error",
+      [](std::set<l6e::net::url> &, l6e::net::url const &) -> p7r::error_t
+      {
+        // Actually, very expected.
+        return p7r::ERR_UNEXPECTED;
+      }
+  );
+  ASSERT_EQ(p7r::ERR_SUCCESS, err);
+
+  err = api->resolver().resolve(results, url);
+  ASSERT_EQ(p7r::ERR_UNEXPECTED, err);
+}
